192becd24d2f6164b143287395ae54e6b6acb503
Patrick Connell <connell.patrick@gmail.com>
2026-08-11 11:25:58 +0100

    Interactive Conditional Logic App

--- Prog_Fundamentals/Module 3 Labs/Lab 3 Conditional Logic/Task1.cpp
+++ Prog_Fundamentals/Module 3 Labs/Lab 3 Conditional Logic/Task1.cpp
@@ -7,42 +7,42 @@ using namespace std;
 
 int main() {
 
-    string playerChoice;
+    string playerAction;
 
-    playerChoice = "make 1st decision"; // set playerChoice to "make decision" to allow for using a
-    // while loop in order to issue correction upon invalid choice.
+    playerAction = "make 1st decision"; // set playerChoice to "make decision" to allow for using a
+    // while loop in order to issue correction upon invalid decision.
 
     cout << "Welcome to the Adventure Game!" << endl;
     cout << "You stand at a crossroads in a mysterious forest." << endl;
     cout << "Do you want to go 'left', 'right' or 'forward'? ";
 
-    while (playerChoice == "make 1st decision") { 
+    while (playerAction == "make 1st decision") { 
         
         // create while loop to allow for incorrect entry
         // and correction
 
-        cin >> playerChoice; // input player choice
-        transform(playerChoice.begin(), playerChoice.end(), playerChoice.begin(), ::tolower);
+        cin >> playerAction; // input player decision
+        transform(playerAction.begin(), playerAction.end(), playerAction.begin(), ::tolower);
         // use this to convert to lowercase letters. It allows for uppercase entries and
         // words having a mix of upper and lowercase letters.
 
     
-        if (playerChoice == "left") {
+        if (playerAction == "left") {
             cout << "\nYou discover a hidden treasure chest!" << endl;
             cout << "Inside you find 100 gold coins." << endl;
             break; // use a break statement to break out of if statement and while loop
-            // upon a valid choice being implemented.
+            // upon a valid decision being implemented.
             // https://cppreference.com/cpp/language/break
         } 
     
     
-        else if (playerChoice == "right") {
+        else if (playerAction == "right") {
             cout << "\nYou meet a wise old sage." << endl;
             cout << "The sage gives you a magical potion." << endl;
             break;
         } 
 
-        else if (playerChoice == "forward") {
+        else if (playerAction == "forward") {
             cout << "\nYou have chosen wisely, forsaking gold and magical potions." << endl;
             cout << "I give you the key to a door hidden in the forest." << endl;
             break;
@@ -50,34 +50,34 @@ int main() {
     
         
         else { 
-            cout << "\nYour choice is not a valid one." << endl;
-            cout << "You stand still, unsure of your choice." << endl;
+            cout << "\nYour action is not a valid one." << endl;
+            cout << "You stand still, unsure of your decision." << endl;
             cout << "Time passes and nothing happens." << endl;
             cout << "Choose from 'left', 'right', or 'forward'! ";
     
         }
 
-        playerChoice = "make 1st decision";
+        playerAction = "make 1st decision";
          
     }
 
-    playerChoice = "make 2nd decision"; // set playerChoice to "next decision" to allow for using a
+    playerAction = "make 2nd decision"; // set playerChoice to "next decision" to allow for using a
     // while loop in order to issue correction upon invalid choice.
 
-    cout << "\nYou now have to make a second choice. " << endl;
+    cout << "\nYou now have to make a second decision. " << endl;
     cout << "You can either go left, right or forward." << endl;
     cout << "You cannot stand still or go backwards. " << endl;
-    cout << "What is your choice? ";
+    cout << "What is your decision? ";
 
-    while (playerChoice == "make 2nd decision") {
+    while (playerAction == "make 2nd decision") {
 
         // create while loop to allow for incorrect entry
         // and correction. The same logic as that applied above.
 
-        cin >> playerChoice;
-        transform(playerChoice.begin(), playerChoice.end(), playerChoice.begin(), ::tolower);
+        cin >> playerAction;
+        transform(playerAction.begin(), playerAction.end(), playerAction.begin(), ::tolower);
 
-        if (playerChoice == "left") {
+        if (playerAction == "left") {
 
             cout << "\nYou have gained 50 bonus points." << endl;
             cout << "You need to find a key to open a hidden door in the forest. " << endl;
@@ -87,7 +87,7 @@ int main() {
 
         }
 
-        if (playerChoice == "right") {
+        if (playerAction == "right") {
 
             cout << "\nYou have gained 50 bonus points." << endl;
             cout << "You need to find a key to open a hidden door in the forest. " << endl;
@@ -97,7 +97,7 @@ int main() {
 
         }
 
-        if (playerChoice == "forward") {
+        if (playerAction == "forward") {
 
             cout << "\nYou have gained 50 bonus points." << endl;
             cout << "You need to find a key to open a hidden door in the forest. " << endl;
@@ -108,14 +108,14 @@ int main() {
         } 
 
         else { 
-            cout << "\nYour choice is not a valid one." << endl;
+            cout << "\nYour decision is not a valid one." << endl;
             cout << "You stand still, unsure of your choice." << endl;
             cout << "Time passes and nothing happens." << endl;
             cout << "Choose from 'left', 'right', or 'forward', but not 'backwards'" << endl;
             cout << "or staying put! ";
         }
 
-        playerChoice = "make 2nd decision";
+        playerAction = "make 2nd decision";
         
 
     }
